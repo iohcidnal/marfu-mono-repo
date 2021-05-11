5ed71ee7d27af32065babff648d6b6b93d198281
iohcidnal <landicho.ian@gmail.com>
2021-05-10 19:52:18 -0600

    Add dashboard header

--- packages/client/src/pages/dashboard.tsx
+++ packages/client/src/pages/dashboard.tsx
@@ -1,5 +1,17 @@
 import { GetServerSideProps } from 'next';
-import { Box, Wrap, WrapItem, Text } from '@chakra-ui/react';
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  IconButton,
+  Box,
+  Wrap,
+  WrapItem,
+  Text,
+  HStack
+} from '@chakra-ui/react';
+import { HamburgerIcon, AddIcon } from '@chakra-ui/icons';
 
 import { IMemberDto, IMedicationDto } from '@common';
 
@@ -20,27 +32,47 @@ const colorMap = {
 
 export default function Dashboard({ medications }: DashboardProps) {
   return (
-    <Wrap p="10" justify="center" alignContent="flex-start">
-      {medications.map(med => {
-        return (
-          <WrapItem key={med._id}>
-            <Box
-              w="xs"
-              borderWidth="1px"
-              borderRadius="lg"
-              p="4"
-              as="button"
-              shadow="md"
-              {...colorMap[med.status]}
-            >
-              <Text fontSize="lg" fontWeight="semibold">
-                {med.firstName} {med.lastName}
-              </Text>
-            </Box>
-          </WrapItem>
-        );
-      })}
-    </Wrap>
+    <>
+      <Box p="4" shadow="md">
+        <HStack>
+          <Menu>
+            <MenuButton
+              as={IconButton}
+              aria-label="Options"
+              icon={<HamburgerIcon />}
+              variant="outline"
+            />
+            <MenuList>
+              <MenuItem icon={<AddIcon />}>Add new member</MenuItem>
+            </MenuList>
+          </Menu>
+          <Text fontSize="lg" fontWeight="semibold" color="gray.600">
+            Dashboard
+          </Text>
+        </HStack>
+      </Box>
+      <Wrap p="10" justify="center" alignContent="flex-start">
+        {medications.map(med => {
+          return (
+            <WrapItem key={med._id}>
+              <Box
+                w="xs"
+                borderWidth="1px"
+                borderRadius="lg"
+                p="4"
+                as="button"
+                shadow="md"
+                {...colorMap[med.status]}
+              >
+                <Text fontSize="lg" fontWeight="semibold">
+                  {med.firstName} {med.lastName}
+                </Text>
+              </Box>
+            </WrapItem>
+          );
+        })}
+      </Wrap>
+    </>
   );
 }
 
