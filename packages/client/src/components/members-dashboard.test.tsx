1ed768c055747b419186a6ea163eab0bc4d7c508
iohcidnal <landicho.ian@gmail.com>
2021-05-15 14:28:30 -0600

    Add members dashboard snapshot test

---
+++ packages/client/src/components/members-dashboard.test.tsx
@@ -0,0 +1,17 @@
+import { act } from '@testing-library/react';
+import { renderWithProviders } from '../utils';
+import MembersDashboard from './members-dashboard';
+
+const render = renderWithProviders(<MembersDashboard medications={[]} />);
+
+describe('SignIn', () => {
+  it('should render properly', async () => {
+    let rendered;
+    await act(async () => {
+      rendered = render();
+    });
+    expect(rendered.asFragment()).toMatchSnapshot();
+  });
+
+  it.todo('cover other lines');
+});
