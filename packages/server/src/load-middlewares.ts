94dc323cff2aac09d4ad0dca298f9c0a14dc42c9
iohcidnal <landicho.ian@gmail.com>
2021-07-25 19:42:11 -0600

    Add APPHOST

--- packages/server/src/load-middlewares.ts
+++ packages/server/src/load-middlewares.ts
@@ -8,7 +8,12 @@ export default function loadMiddlewares() {
   const app = express();
 
   app.use(express.json());
-  app.use(cors({ credentials: true, origin: ['http://localhost:3000'] })); // TODO: Limit cors origin in production
+  app.use(
+    cors({
+      credentials: true,
+      origin: [process.env.APPHOST as string]
+    })
+  );
   app.use(cookieParser());
   app.use(
     session({
